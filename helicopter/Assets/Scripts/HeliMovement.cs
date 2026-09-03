569f2e6eb2428903f3d39e0afb0e1f4fb05a8983
JuiceJ <joosjae01@gmail.com>
2026-09-03 09:33:09 +0900

    [Feat] 헬리콥터 움직임 구현

---
+++ helicopter/Assets/Scripts/HeliMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliMovement : MonoBehaviour
+{
+    [SerializeField] private float _heliSpeed;
+    [SerializeField] private float _rotateSpeed;
+    void Update()
+    {
+        Vector3 direction = GetDirection();
+        HeliRotate(direction);
+        Move(direction);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        if (Input.GetKey(KeyCode.Space) && direction.y <= 500)
+        {
+            direction.y += 1f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && direction.y >= 0)
+        {
+            direction.y -= 1f;
+
+        }
+
+        transform.Translate(0, direction.y * _heliSpeed * Time.deltaTime, direction.z * _heliSpeed * Time.deltaTime);
+    }
+
+    private void HeliRotate(Vector3 direction)
+    {
+        if (direction == Vector3.zero) return;
+        transform.Rotate(0, direction.x * _rotateSpeed* Time.deltaTime, 0);
+    }
+
+    private Vector3 GetDirection()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, Vertical);
+        return direction.normalized;
+    }
+}
